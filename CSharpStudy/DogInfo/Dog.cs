370c5b7a55d084d6f3606349c5164648a714fc68
Jaim Koo <iamjikoo@gmail.com>
2020-10-22 20:53:13 -0400

    Add Project: Dog

---
+++ CSharpStudy/DogInfo/Dog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogInfo
+{
+    public class Dog
+    {
+        // Field
+        private string name;
+        private string breed;
+        private string colour;
+        int sex;
+
+        // Constructor
+        //public Dog(string name)
+        //{
+        //    //name = string.Empty;
+        //    this.name = name;
+        //}              
+
+        // Property
+        //public string Name
+        //{
+        //    get { return this.name; }
+        //    set { this.name = value; }
+        //}
+        public string Name { get; set; }
+
+        //public string Breed
+        //{
+        //    get { return this.breed; }
+        //    set { this.breed = value; }
+        //}
+        public string Breed { get; set; }
+
+        public string Colour
+        {
+            get { return this.colour; }
+            set { this.colour = value; }
+        }
+
+        public int Sex
+        {
+            get { return this.sex; }
+            set { this.sex = value; }
+        }
+
+        // method
+        public string GetDogData()
+        {
+            string data = string.Format("Name: {0} Breed: {1} Colour: {2} Sex: {3}",
+                this.name, this.breed, this.colour, this.sex);
+            return data;
+        }
+    }
+}
---
+++ CSharpStudy/DogInfo/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DogInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu;
+            string name;
+            string breed;
+            int sex;
+
+            List<Dog> Dogs = new List<Dog>();
+
+            Console.WriteLine("Welcome to the Dog Information System");
+
+            // List, Hashset, Queue, Stack
+
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("A. Display the name, breed, colour, and sex of the doay");
+                Console.WriteLine("B. Add a new dog");
+                Console.WriteLine("C. Edit an existing dog");
+                Console.WriteLine("D. Display data from class");
+                Console.WriteLine("E. Exit the program");
+                Console.Write("\nSelect menu: ");
+
+                menu = Console.ReadLine();
+
+                switch (menu)
+                {
+                    case "A": case "a":
+                        foreach (Dog dog in Dogs)
+                        {
+                            Console.WriteLine("Name: {0} Breed: {1} Sex: {2}", dog.Name, dog.Breed, dog.Sex);
+                        }
+                        //Dog newDog = new Dog();
+                        // add a new dog
+                        break;
+
+                    case "B": case "b":
+                        //Console.Write("Name: ");
+
+                        //name = Console.ReadLine();
+                        //newDog.Name = name;
+
+                        Dog newDog = new Dog();
+
+                        Console.Write("Name: ");
+
+                        name = Console.ReadLine();
+                        newDog.Name = name;
+
+                        Console.Write("Breed: ");
+                        breed = Console.ReadLine();
+                        newDog.Breed = breed;
+
+                        //Console.Write("Colour: ");
+                        //colour = Console.ReadLine();
+
+                        Console.Write("Sex 1: female, 2: male: ");
+                        sex = int.Parse(Console.ReadLine());
+                        newDog.Sex = sex;
+
+                        Dogs.Add(newDog);
+
+                        // edit dog 
+                        break;
+
+                    case "C": case "c":
+                        // display data
+                        
+                        break;
+
+                    case "D": case "d":
+                        break;
+
+                    case "E": case "e":
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid menu: {0}", menu);
+                        break;
+                }
+
+            } // while
+        } // main
+    }
+}
